42cd9b2f7c834c74b6a10341b3e4fa2b25a064ad
Niklas Kraßnig <niklas@krassnig.dev>
2022-10-25 16:13:31 +0200

    Add Scharf as search suggestion

--- react/src/components/SearchSuggestions.tsx
+++ react/src/components/SearchSuggestions.tsx
@@ -5,7 +5,7 @@ interface Props {
 	setSearch: (value: string) => void;
 }
 
-const searchSuggestions = ['*', 'Vegan', 'Vegetarisch', 'Fleisch', 'Nudeln', 'Reis', 'Kartoffel', 'Süß', 'Salat', 'Suppe'];
+const searchSuggestions = ['*', 'Vegan', 'Vegetarisch', 'Fleisch', 'Nudeln', 'Reis', 'Kartoffel', 'Süß', 'Scharf', 'Salat', 'Suppe'];
 
 export const SearchSuggestions: React.FC<Props> = props => {
 	const { setSearch } = props;
